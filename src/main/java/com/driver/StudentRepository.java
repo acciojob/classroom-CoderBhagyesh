e37d9da8abf246e1897479ed51c8021606c8e273
Bhagyesh Khobragade <bhagyeshgk143@gmail.com>
2023-10-05 18:43:52 +0530

    Attempt 4

--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -8,61 +8,60 @@ import java.util.List;
 
 @Repository
 public class StudentRepository {
-    HashMap<String, Student> studentHashMap = new HashMap<>();
-    HashMap<String, Teacher> teacherHashMap = new HashMap<>();
-    HashMap<String, List<String>> studentsCorrespondsTeacher = new HashMap<String, List<String>>();
 
 
-    public void addStudent(Student student) {
-        studentHashMap.put(student.getName(), student);
+    HashMap<String,Student> studentHashMap=new HashMap<>();
+    HashMap<String,Teacher> teacherHashMap=new HashMap<>();
+    HashMap<String,List<String>> mapDB=new HashMap<>();
+
+    public void addStudent(Student student){
+        studentHashMap.put(student.getName(),student);
     }
-
-    public void addTeacher(Teacher teacher) {
-        teacherHashMap.put(teacher.getName(), teacher);
+    public  void addTeacher(Teacher teacher){
+        teacherHashMap.put(teacher.getName(),teacher);
     }
-
-    public void addStudentTeacherPair(String student, String teacher) {
-        List<String> students = studentsCorrespondsTeacher.getOrDefault(teacher, new ArrayList<>());
-        students.add(student);
-        studentsCorrespondsTeacher.put(teacher, students);
+    public void addStudentTeacherPair(String student ,String teacher){
+        List<String> slist=mapDB.containsKey(teacher)==true ?mapDB.get(teacher):new ArrayList<>();
+        slist.add(student);
+        mapDB.put(teacher,slist);
     }
-
-    public Student getStudentByName(String name) {
+    public Student getStudentByName(String name){
         return studentHashMap.get(name);
     }
-
-    public Teacher getTeacherByName(String name) {
+    public Teacher getTeacherByName(String name){
         return teacherHashMap.get(name);
     }
-
-    public List<String> getStudentsByTeacherName(String teacher) {
-        return studentsCorrespondsTeacher.get(teacher);
+    public List<String> getStudentsByTeacherName(String name){
+        return mapDB.get(name);
     }
-
-    public List<String> getAllStudents() {
-        List<String> students = new ArrayList<>();
-        for(String student: studentHashMap.keySet()) {
-            students.add(student);
+    public List<String> getAllStudents(){
+        List<String> ansList=new ArrayList<>();
+        for(String student:studentHashMap.keySet()){
+            ansList.add(student);
         }
-        return students;
+        return ansList;
     }
-
-    public void deleteTeacherByName(String teacher) {
+    public void deleteTeacherByName(String teacher){
+        List<String> slist=mapDB.get(teacher);
+        for(String name:slist){
+            studentHashMap.remove(name);
+        }
         teacherHashMap.remove(teacher);
+        mapDB.remove(teacher);
     }
-
     public void deleteAllTeachers() {
         for(String teacher:teacherHashMap.keySet()){
-            if(studentsCorrespondsTeacher.containsKey(teacher)) {
-                List<String> stringList = studentsCorrespondsTeacher.get(teacher);
+            if(mapDB.containsKey(teacher)) {
+                List<String> stringList = mapDB.get(teacher);
                 for(String student:stringList) {
                     if (studentHashMap.containsKey(student)){
                         studentHashMap.remove(student);
                     }
                 }
-                studentsCorrespondsTeacher.remove(teacher);
+                mapDB.remove(teacher);
             }
             teacherHashMap.remove(teacher);
         }
     }
+
 }
